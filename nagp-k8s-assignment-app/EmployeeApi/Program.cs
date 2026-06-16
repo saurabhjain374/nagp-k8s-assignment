45f29b981580f0cb1da014642ea9c7f953be134c
Saurabh Jain <saurabhjain374@gmail.com>
2026-06-16 13:55:54 +0530

    update

--- nagp-k8s-assignment-app/EmployeeApi/Program.cs
+++ nagp-k8s-assignment-app/EmployeeApi/Program.cs
@@ -26,11 +26,10 @@ namespace EmployeeApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+           
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
